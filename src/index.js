aabcd67d895b94f277f26941966e58bf7c74f143
huangziyi01 <huangziyi01@baidu.com>
2016-02-09 21:51:10 +0800

    quick push

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import $ from 'jquery'
 import 'fit-style'
 
 import Layout from './layout'
@@ -8,11 +7,3 @@ import Section from './section'
 import Footer from './footer'
 
 export { Layout, Header, Sidebar, Section, Footer }
-
-// 设置全局样式
-$('html,body').css({
-    width: '100%',
-    height: '100%',
-    overflow: 'hidden',
-    margin: 0
-})
--- src/layout/index.js
+++ src/layout/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createStore } from 'redux'
 import LayoutApp from '../reducers'
+import $ from 'jquery'
 import './index.scss'
 
 export default class Layout extends React.Component {
@@ -9,9 +10,27 @@ export default class Layout extends React.Component {
         this.state = {}
     }
 
+    componentWillMount() {
+        // 设置全局样式
+        $('html,body').css({
+            width: '100%',
+            height: '100%',
+            overflow: 'hidden'
+        })
+    }
+
+    componentWillUnmount() {
+        // 设置全局样式
+        $('html,body').css({
+            width: 'auto',
+            height: 'auto',
+            overflow: 'auto'
+        })
+    }
+
     render() {
         let store = createStore(LayoutApp)
-        
+
         let childs = React.Children.map(this.props.children, (children, index)=> {
             return React.cloneElement(children, {
                 key: index,
