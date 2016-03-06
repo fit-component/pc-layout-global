15dfd57e9b23b8c616d75dbe939a7289e068a350
huangziyi01 <huangziyi01@baidu.com>
2016-03-06 19:26:39 +0800

    quick push

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,13 +3,24 @@ import { shallow, mount } from 'enzyme'
 import { Layout, Header , Sidebar, Section, Footer } from 'fit-layout-global'
 
 describe('fit-layout-global', ()=> {
-    it('样式没想好怎么测', ()=> {
+    it('样式没想好怎么测~', ()=> {
         const node = mount(
             <Layout>
                 <Sidebar width="60">侧边栏</Sidebar>
                 <Section>主体</Section>
             </Layout>
         )
+        expect(true).to.equal(true)
+    })
+
+    it('unmount', ()=> {
+        const node = mount(
+            <Layout>
+                <Sidebar width="60">侧边栏</Sidebar>
+                <Section>主体</Section>
+            </Layout>
+        )
+        node.unmount()
         expect(true).to.equal(true)
     })
 })
